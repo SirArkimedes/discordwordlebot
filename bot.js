0c98fbcb42dfce9342eb121654d67441864b1021
Andrew Robinson <sirarkimedes@gmail.com>
2022-01-06 18:07:34 -0700

    Basic leaderboard built

--- bot.js
+++ bot.js
@@ -2,9 +2,15 @@ const { Client, Intents } = require('discord.js');
 
 const auth = require('./auth.json');
 const commands = require('./commands.js');
+const leaderboard = require('./leaderboard.js');
 const { scheduleRollCall } = require('./rollcall.js');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES
+  ]
+});
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -12,8 +18,9 @@ client.on('ready', () => {
   scheduleRollCall(client);
 });
 
-client.on('message', message => {
+client.on('messageCreate', message => {
   commands.attemptCommandEvaluation(message);
+  leaderboard.parseMessage(message);
 });
 
 client.login(auth.token);
---
+++ leaderboard.js
@@ -0,0 +1,49 @@
+const { MessageEmbed } = require('discord.js');
+
+const { readInFile, writeFile } = require('./file_reader.js');
+
+const LEADERBOARD_FILE_PATH = './leaderboard_stats.json'
+
+// Public
+
+function parseMessage(message) {
+    // https://github.com/kevintrankt/discord-wordle-bot/blob/main/index.ts#L258
+    const wordleRegex = /Wordle \d{3} ([\dX])\/6\n{0,2}[⬛🟩🟨⬜]{5}/;
+    const wordleMessage = message.content.match(wordleRegex);
+
+    if (wordleMessage) {
+        console.log(wordleMessage);
+
+        updateLeaderboard(message, wordleMessage);
+    }
+}
+
+exports.parseMessage = parseMessage;
+
+// Helpers
+
+function updateLeaderboard(message, wordleMessage) {
+    readInFile(LEADERBOARD_FILE_PATH, data => {
+        const leaderboard = JSON.parse(data);
+
+        let score;
+        if (wordleMessage[1] == 'X') {
+            score = 0;
+        } else {
+            score = 7 - parseInt(wordleMessage[1]);
+        }
+        
+        var currentScore = leaderboard[message.author.id];
+        if (currentScore == undefined) {
+            currentScore = 0;
+        }
+        currentScore += score;
+        leaderboard[message.author.id] = currentScore;
+
+        writeFile(LEADERBOARD_FILE_PATH, JSON.stringify(leaderboard, null, '\t'), succeeded => {
+            if (succeeded) {
+                console.log("Leaderboard updated!");
+            }
+        });
+    });
+}
